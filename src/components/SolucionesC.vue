bc1310cceaaf38dffeacd2a5b30902167ad93d7e
albertoramil <alberto.ramil@norvoz.es>
2019-10-16 17:00:48 +0200

    Mostrar un json en un card

--- src/components/SolucionesC.vue
+++ src/components/SolucionesC.vue
@@ -56,121 +56,96 @@ export default {
   
 <template>
   <div>
-    <v-data-table :headers="headers" :items="desserts" :items-per-page="5" class="elevation-1"></v-data-table>
+    <v-card>
+      <v-btn color="indigo" class="white--text" @click="aconsulta">Carga Todo</v-btn>
 
-    <v-data-table :headers="headers" :items="desserts">
-      <template v-slot:items="props">
-        <td class="text-xs-right">{{ props.item.name }}</td>
-        <td class="text-xs-right">{{ props.item.calories }}</td>
-        <td class="text-xs-right">{{ props.item.fat }}</td>
-        <td class="text-xs-right">{{ props.item.carbs }}</td>
-        <td class="text-xs-right">{{ props.item.protein }}</td>
-        <td class="text-xs-right">{{ props.item.iron }}</td>
-      </template>
-    </v-data-table>
+      <v-card-title>
+        Albunes
+        <br />
+        <br />
+        <br />
+        <br />
+
+        <v-text-field v-model="search" label="   Busqueda por cualquier campo"></v-text-field>
+      </v-card-title>
+
+      <v-data-table :headers="cabecera2" :items="info" :search="search">
+        <template v-slot:items="props">
+          <td>{{ props.item.title }}</td>
+          <td>{{ props.item.url }}</td>
+          <td>{{ props.item.albumId }}</td>
+        </template>
+      </v-data-table>
+      <v-btn color="indigo" class="white--text" @click="consultaUnAlbum">Carga un album</v-btn>
+      <br />
+    </v-card>
+
+    <template>
+      <!--<v-card
+    max-width="344"
+    esto es para el tamaño del card
+      >-->
+      <v-card>
+        <v-card-text>
+          <p class="display-1 text--primary">{{"Titulo: "}}{{album.title}}</p>
+          <div class="text--primary">
+            {{"Url: "}}{{album.url}}
+            <br />{{"ThumbnailUrl: " }}{{album.thumbnailUrl}}
+          </div>
+        </v-card-text>
+      </v-card>
+    </template>
   </div>
 </template>
+
+
+   
 
 <script>
 export default {
   data() {
     return {
-      headers: [
+      search: "",
+      info: [],
+      status: "",
+      album: "",
+
+      cabecera2: [
         {
-          text: "Dessert (100g serving)",
+          text: "Título",
           align: "left",
           sortable: false,
-          value: "name"
+          value: "title"
         },
-        { text: "Calories", value: "calories" },
-        { text: "Fat (g)", value: "fat" },
-        { text: "Carbs (g)", value: "carbs" },
-        { text: "Protein (g)", value: "protein" },
-        { text: "Iron (%)", value: "iron" }
-      ],
-      desserts: [
-        {
-          name: "Frozen Yogurt",
-          calories: 159,
-          fat: 6.0,
-          carbs: 24,
-          protein: 4.0,
-          iron: "1%"
-        },
-        {
-          name: "Ice cream sandwich",
-          calories: 237,
-          fat: 9.0,
-          carbs: 37,
-          protein: 4.3,
-          iron: "1%"
-        },
-        {
-          name: "Eclair",
-          calories: 262,
-          fat: 16.0,
-          carbs: 23,
-          protein: 6.0,
-          iron: "7%"
-        },
-        {
-          name: "Cupcake",
-          calories: 305,
-          fat: 3.7,
-          carbs: 67,
-          protein: 4.3,
-          iron: "8%"
-        },
-        {
-          name: "Gingerbread",
-          calories: 356,
-          fat: 16.0,
-          carbs: 49,
-          protein: 3.9,
-          iron: "16%"
-        },
-        {
-          name: "Jelly bean",
-          calories: 375,
-          fat: 0.0,
-          carbs: 94,
-          protein: 0.0,
-          iron: "0%"
-        },
-        {
-          name: "Lollipop",
-          calories: 392,
-          fat: 0.2,
-          carbs: 98,
-          protein: 0,
-          iron: "2%"
-        },
-        {
-          name: "Honeycomb",
-          calories: 408,
-          fat: 3.2,
-          carbs: 87,
-          protein: 6.5,
-          iron: "45%"
-        },
-        {
-          name: "Donut",
-          calories: 452,
-          fat: 25.0,
-          carbs: 51,
-          protein: 4.9,
-          iron: "22%"
-        },
-        {
-          name: "KitKat",
-          calories: 518,
-          fat: 26.0,
-          carbs: 65,
-          protein: 7,
-          iron: "6%"
-        }
+        { text: "url", value: "url" },
+        { text: "album", value: "albumId" }
       ]
     };
+  },
+  methods: {
+    async aconsulta() {
+      try {
+        const baseURI = "https://jsonplaceholder.typicode.com/photos";
+        let respuesta = await this.$axios.get(baseURI);
+        this.info = respuesta.data;
+        this.status = respuesta.status;
+        console.log(respuesta.status);
+      } catch (error) {
+        console.log("error");
+        console.log(error);
+        console.log("error");
+      }
+    },
+    async consultaUnAlbum() {
+      try {
+        const baseURI = "https://jsonplaceholder.typicode.com/photos";
+        let respuesta = await this.$axios.get(baseURI);
+        var albumConcreto = respuesta.data[5];
+        this.album = albumConcreto;
+      } catch (error) {
+        console.log(error);
+      }
+    }
   }
 };
 </script>
